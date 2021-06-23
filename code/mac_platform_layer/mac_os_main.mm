fc16537f3115b9d92ddd85ad142208f03ca15ef6
Theodore Bendixson <tedbendixson@me.com>
2021-06-23 16:11:29 -0600

    Drawing a basic window

--- code/mac_platform_layer/mac_os_main.mm
+++ code/mac_platform_layer/mac_os_main.mm
@@ -1,5 +1,7 @@
 
 #import <AppKit/AppKit.h>
+#import <Metal/Metal.h>
+#import <MetalKit/MetalKit.h>
 
 #include "mac_os_main.h"
 #include "mac_window.mm"
@@ -18,5 +20,34 @@ int main(int argc, const char * argv[])
 {
     MainWindowDelegate *WindowDelegate = [[MainWindowDelegate alloc] init];
 
-    NSRect InitialFrame = NSMakeRect(0, 0, 1024, 1024);
+    r32 GlobalRenderWidth = 1024;
+    r32 GlobalRenderHeight = 1024;
+
+    NSRect InitialFrame = NSMakeRect(0, 0, GlobalRenderWidth, GlobalRenderHeight);
+
+    Window = [[PixelShaderDemoWindow alloc] 
+                initWithContentRect: InitialFrame
+                styleMask: NSWindowStyleMaskTitled |
+                           NSWindowStyleMaskClosable
+                  backing: NSBackingStoreBuffered
+                    defer: NO];    
+    
+    [Window setBackgroundColor: NSColor.blackColor];
+    [Window setTitle: @"Metal Scaling Pixel Art Shader Demo"];
+
+    [Window makeKeyAndOrderFront: nil];
+    [Window setDelegate: WindowDelegate];
+
+    MTKView *MetalKitView = [[MTKView alloc] init];
+    MetalKitView.frame = CGRectMake(0, 0, 
+                                    GlobalRenderWidth, GlobalRenderHeight); 
+
+    MetalKitView.device = MTLCreateSystemDefaultDevice(); 
+    MetalKitView.framebufferOnly = false;
+    MetalKitView.layer.contentsGravity = kCAGravityCenter;
+    MetalKitView.preferredFramesPerSecond = 60;
+
+    [Window setContentView: MetalKitView];
+
+    return NSApplicationMain(argc, argv);
 }
